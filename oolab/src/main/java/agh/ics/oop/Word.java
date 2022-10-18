acd539b4a906bfa8bd5b4bc35e286963355a34ad
Malgorzata Krupanek <malgorzata.krupanek@gmail.com>
2022-10-18 14:55:33 +0200

    adding lab2

--- oolab/src/main/java/agh/ics/oop/Word.java
+++ oolab/src/main/java/agh/ics/oop/Word.java
@@ -39,6 +39,18 @@ public class Word {
         run(directions);
         System.out.println("system zakończył działanie");
 
+        //lab2
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+
+        System.out.println("North to " + MapDirection.NORTH.toString());
+        System.out.println("Po NORTH jest " + MapDirection.NORTH.next());
+        System.out.println("Przed NORTH jest " + MapDirection.NORTH.previous());
+        System.out.println("Wektor NORTH to " + MapDirection.NORTH.toUnitVector());
+
     }
 
 }
